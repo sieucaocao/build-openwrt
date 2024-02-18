754b0d5e167f9fb052dc26f8f1f2a2669ce2640e
sieucaocao <144571307+sieucaocao@users.noreply.github.com>
2024-02-18 15:54:10 +0700

    Create dl_sdk.sh

---
+++ scripts/immortalwrt/dl_sdk.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+###############################################################
+# Author(s): sieucaocao | https://github.com/sieucaocao       #
+###############################################################
+
+# File info
+# FILE_NAME="openwrt-sdk-18.06-SNAPSHOT-ramips-mt7620_gcc-7.3.0_musl.Linux-x86_64"
+# FILE_URL="https://downloads.openwrt.org/releases/18.06-SNAPSHOT/targets/ramips/mt7620/$FILE_NAME.tar.xz"
+FILE_NAME="immortalwrt-sdk-18.06-SNAPSHOT-ar71xx-generic_gcc-8.4.0_musl.Linux-x86_64"
+FILE_URL="https://downloads.immortalwrt.org/releases/18.06-SNAPSHOT/targets/ar71xx/generic/$FILE_NAME.tar.xz"
+
+# Download and extract file(s)
+wget $FILE_URL
+tar -xvf $FILE_NAME.tar.xz
+mv $FILE_NAME sdk
