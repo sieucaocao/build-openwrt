f61f394dc66fa01875881c1a35e29c886f892ffa
sieucaocao <144571307+sieucaocao@users.noreply.github.com>
2024-02-18 21:55:44 +0700

    Delete configs/immortalwrt/imagebuilder directory

--- configs/immortalwrt/imagebuilder/mac1200r.sh
+++
@@ -1,3 +0,0 @@
-make image \
-     PROFILE=mc-mac1200r \
-     PACKAGES="ath10k-firmware-qca988x base-files busybox ca-bundle default-settings-chn dnsmasq-full dropbear firewall fstools iptables ipv6helper kmod-ath10k kmod-ath9k kmod-gpio-button-hotplug kmod-ipt-raw kmod-nf-nathelper kmod-nf-nathelper-extra kmod-usb2 libc libgcc libustream-openssl logd luci luci-app-turboacc luci-compat luci-lib-base luci-lib-fs luci-lib-ipkg mtd netifd opkg ppp ppp-mod-pppoe procd swconfig uboot-envtools uboot-envtools uci uclient-fetch urandom-seed urngd wpad-openssl luci-app-zerotier luci-app-pppoe-server"
