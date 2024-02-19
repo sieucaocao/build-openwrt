fcb27bb1cbf619a20846c8206056f6d1aa941159
sieucaocao <144571307+sieucaocao@users.noreply.github.com>
2024-02-19 11:44:23 +0700

    Update dl_imagebuilder.sh

--- scripts/openwrt/dl_imagebuilder.sh
+++ scripts/openwrt/dl_imagebuilder.sh
@@ -6,8 +6,8 @@
 # File info
 # FILE_NAME="openwrt-imagebuilder-22.02-SNAPSHOT-ramips-mt7620.Linux-x86_64.tar.xz"
 # FILE_URL="https://downloads.openwrt.org/releases/22.02-SNAPSHOT/targets/ramips/mt7620/$FILE_NAME.tar.xz"
-FILE_NAME="openwrt-imagebuilder-22.03-SNAPSHOT-ramips-mt7620.Linux-x86_64"
-FILE_URL="https://downloads.openwrt.org/releases/22.03-SNAPSHOT/targets/ramips/mt7620/$FILE_NAME.tar.xz"
+FILE_NAME="openwrt-imagebuilder-19.07.10-ar71xx-generic.Linux-x86_64"
+FILE_URL="https://downloads.openwrt.org/releases/19.07.10/targets/ar71xx/generic/$FILE_NAME.tar.xz"
 
 # Download and extract file(s)
 wget $FILE_URL
