51d5d69de1606097a27ffab08db87c0e808f6fff
sieucaocao <144571307+sieucaocao@users.noreply.github.com>
2024-02-24 23:02:08 +0700

    Update dl_imagebuilder-2.sh

--- scripts/openwrt/dl_imagebuilder-2.sh
+++ scripts/openwrt/dl_imagebuilder-2.sh
@@ -1,1 +1,16 @@
 
+#!/bin/bash
+###############################################################
+# Author(s): Việt Anh Bùi | https://github.com/vietanhbui2000 #
+###############################################################
+
+# File info
+# FILE_NAME="openwrt-imagebuilder-22.02-SNAPSHOT-ramips-mt7620.Linux-x86_64.tar.xz"
+# FILE_URL="https://downloads.openwrt.org/releases/22.02-SNAPSHOT/targets/ramips/mt7620/$FILE_NAME.tar.xz"
+FILE_NAME="openwrt-imagebuilder-19.07.10-ramips-mt76x8.Linux-x86_64"
+FILE_URL="https://downloads.openwrt.org/releases/19.07.10/targets/ramips/mt76x8/$FILE_NAME.tar.xz"
+
+# Download and extract file(s)
+wget $FILE_URL
+tar -xvf $FILE_NAME.tar.xz
+mv $FILE_NAME imagebuilder
