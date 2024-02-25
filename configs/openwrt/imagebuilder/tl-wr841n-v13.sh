758021fbeb4c96f25fd61351926bf52f4e85ded0
sieucaocao <144571307+sieucaocao@users.noreply.github.com>
2024-02-25 12:59:37 +0700

    Update tl-wr841n-v13.sh

--- configs/openwrt/imagebuilder/tl-wr841n-v13.sh
+++ configs/openwrt/imagebuilder/tl-wr841n-v13.sh
@@ -5,5 +5,5 @@
 
 make image \
      PROFILE=tl-wr841n-v13 \
-     PACKAGES="base-files busybox dnsmasq dropbear firewall fstools -ip6tables iptables kmod-gpio-button-hotplug kmod-gpio-button-hotplug kmod-ipt-offload kmod-leds-gpio kmod-leds-gpio kmod-mt7603 libc libgcc logd luci mtd netifd -odhcp6c -odhcpd-ipv6only opkg ppp ppp-mod-pppoe swconfig uci uclient-fetch urandom-seed urngd wpad-basic"
+     PACKAGES="base-files busybox dnsmasq dropbear firewall fstools -ip6tables iptables kmod-gpio-button-hotplug kmod-gpio-button-hotplug kmod-ipt-offload kmod-leds-gpio kmod-leds-gpio kmod-mt7603 libc libgcc logd luci mtd netifd -odhcp6c -odhcpd-ipv6only opkg ppp ppp-mod-pppoe swconfig uci uclient-fetch urandom-seed urngd wpad-basic htop"
      
