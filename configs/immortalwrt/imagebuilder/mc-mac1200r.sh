aa61aa233d79d959016d1902318b74e7792fb240
sieucaocao <144571307+sieucaocao@users.noreply.github.com>
2024-02-26 22:07:10 +0700

    Update mc-mac1200r.sh

--- configs/immortalwrt/imagebuilder/mc-mac1200r.sh
+++ configs/immortalwrt/imagebuilder/mc-mac1200r.sh
@@ -5,4 +5,4 @@
 
 make image \
      PROFILE=mc-mac1200r \
-     PACKAGES="ath10k-firmware-qca988x base-files busybox ca-bundle default-settings-chn dnsmasq-full dropbear firewall fstools iptables ipv6helper kmod-ath10k kmod-ath9k kmod-gpio-button-hotplug kmod-ipt-raw kmod-nf-nathelper kmod-nf-nathelper-extra kmod-usb2 libc libgcc libustream-openssl logd luci -luci-app-cpufreq -luci-app-filetransfer luci-app-turboacc luci-compat -luci-lib-base luci-lib-fs luci-lib-ipkg mtd netifd opkg ppp ppp-mod-pppoe procd swconfig uboot-envtools uboot-envtools uci uclient-fetch urandom-seed urngd wpad-openssl htop"
+     PACKAGES="ath10k-firmware-qca988x base-files -block-mount busybox ca-bundle default-settings-chn dnsmasq-full dropbear firewall fstools iptables ipv6helper kmod-ath10k kmod-ath9k kmod-gpio-button-hotplug kmod-ipt-raw kmod-nf-nathelper kmod-nf-nathelper-extra kmod-usb2 libc libgcc libustream-openssl logd luci -luci-app-cpufreq -luci-app-filetransfer luci-app-turboacc luci-compat -luci-lib-base luci-lib-fs luci-lib-ipkg mtd netifd opkg ppp ppp-mod-pppoe procd swconfig uboot-envtools uboot-envtools uci uclient-fetch urandom-seed urngd wpad-openssl htop"
