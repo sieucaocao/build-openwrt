61259aef843c629799e99495693a20a7774a8e5c
sieucaocao <144571307+sieucaocao@users.noreply.github.com>
2024-05-18 10:36:55 +0700

    Create dl_imagebuilder2.sh

---
+++ scripts/immortalwrt/dl_imagebuilder2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+###############################################################
+# Author(s): sieucaocao | https://github.com/sieucaocao       #
+###############################################################
+
+# File info
+# FILE_NAME="openwrt-imagebuilder-22.02-SNAPSHOT-ramips-mt7620.Linux-x86_64.tar.xz"
+# FILE_URL="https://downloads.openwrt.org/releases/22.02-SNAPSHOT/targets/ramips/mt7620/$FILE_NAME.tar.xz"
+FILE_NAME="immortalwrt-imagebuilder-18.06-SNAPSHOT-ramips-mt76x8.Linux-x86_64"
+FILE_URL="https://downloads.immortalwrt.org/releases/18.06-SNAPSHOT/targets/ramips/mt76x8/$FILE_NAME.tar.xz"
+
+# Download and extract file(s)
+wget $FILE_URL
+tar -xvf $FILE_NAME.tar.xz
+mv $FILE_NAME imagebuilder
