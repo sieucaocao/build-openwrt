a46d39e8049fef1dabf168f7ad05e7816b16eeca
sieucaocao <144571307+sieucaocao@users.noreply.github.com>
2024-05-22 09:50:10 +0700

    Delete configs/immortalwrt/imagebuilder/mc-mac1200r.sh

--- configs/immortalwrt/imagebuilder/mc-mac1200r.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#####################################################################
-# Original author: Việt Anh Bùi | https://github.com/vietanhbui2000 #
-#####################################################################
-
-make image \
-     PROFILE=mc-mac1200r \
-     PACKAGES="-block-mount -luci -luci-app-filetransfer -luci-app-cpufreq -luci-app-turboacc kmod-fast-classifier kmod-ipt-fullconenat kmod-shortcut-fe htop https-dns-proxy"
