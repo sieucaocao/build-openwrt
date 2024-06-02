68dbbc4c60c0f089d86a52d02bc5b4b57589efaf
sieucaocao <144571307+sieucaocao@users.noreply.github.com>
2024-06-02 22:51:06 +0700

    Update mc-mac1200r.sh

--- configs/openwrt/imagebuilder/mc-mac1200r.sh
+++ configs/openwrt/imagebuilder/mc-mac1200r.sh
@@ -5,4 +5,4 @@
 
 make image \
      PROFILE=mc-mac1200r \
-     PACKAGES="-ath10k-firmware-qca988x-ct ath10k-firmware-qca988x base-files busybox dnsmasq dropbear firewall fstools -ip6tables iptables -kmod-ath10k-ct kmod-ath10k kmod-ath9k kmod-ath9k kmod-gpio-button-hotplug kmod-ipt-offload libc libgcc logd luci mtd netifd -odhcp6c -odhcpd-ipv6only opkg ppp ppp-mod-pppoe swconfig uboot-envtools uci uclient-fetch urandom-seed urngd wpad-basic htop luci-compat"
+     PACKAGES="-ath10k-firmware-qca988x-ct ath10k-firmware-qca988x -kmod-ath10k-ct kmod-ath10k"
