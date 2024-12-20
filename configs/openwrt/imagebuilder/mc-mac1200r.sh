ecb60f633f655058e48f8cf16a8d15870908baa5
sieucaocao <144571307+sieucaocao@users.noreply.github.com>
2024-12-21 03:29:25 +0700

    Update mc-mac1200r.sh

--- configs/openwrt/imagebuilder/mc-mac1200r.sh
+++ configs/openwrt/imagebuilder/mc-mac1200r.sh
@@ -4,6 +4,6 @@
 #####################################################################
 
 make image \
-     FILE="file" \
+     FILE="files" \
      PROFILE=mc-mac1200r \
      PACKAGES="-ath10k-firmware-qca988x-ct ath10k-firmware-qca988x -kmod-ath10k-ct kmod-ath10k -dnsmasq dnsmasq-full"
