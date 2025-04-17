720c2346cfabe0aed2960a1c4b6b41484168163f
sieucaocao <144571307+sieucaocao@users.noreply.github.com>
2025-04-17 22:11:39 +0700

    Update mc-mac1200r-2.sh

--- configs/openwrt/imagebuilder/mc-mac1200r-2.sh
+++ configs/openwrt/imagebuilder/mc-mac1200r-2.sh
@@ -3,7 +3,7 @@
 # Original author: Việt Anh Bùi | https://github.com/vietanhbui2000 #
 #####################################################################
 
-make image \
+make image FORCE=1 \
      FILES=files \
      PROFILE=mc-mac1200r \
      PACKAGES="luci"
