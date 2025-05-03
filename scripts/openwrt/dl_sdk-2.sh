581623d96de19228008c4237c350b627ab387478
sieucaocao <144571307+sieucaocao@users.noreply.github.com>
2025-05-03 22:15:27 +0700

    Update dl_sdk-2.sh

--- scripts/openwrt/dl_sdk-2.sh
+++ scripts/openwrt/dl_sdk-2.sh
@@ -6,8 +6,8 @@
 # File info
 # FILE_NAME="openwrt-sdk-18.06-SNAPSHOT-ramips-mt7620_gcc-7.3.0_musl.Linux-x86_64"
 # FILE_URL="https://downloads.openwrt.org/releases/18.06-SNAPSHOT/targets/ramips/mt7620/$FILE_NAME.tar.xz"
-FILE_NAME="openwrt-sdk-19.07.10-ramips-mt76x8_gcc-7.5.0_musl.Linux-x86_64"
-FILE_URL="https://archive.openwrt.org/releases/19.07.10/targets/ramips/mt76x8/$FILE_NAME.tar.xz"
+FILE_NAME="openwrt-sdk-18.06.9-ar71xx-generic_gcc-7.3.0_musl.Linux-x86_64"
+FILE_URL="https://archive.openwrt.org/releases/18.06.9/targets/ar71xx/generic/$FILE_NAME.tar.xz"
 
 # Download and extract file(s)
 wget $FILE_URL
