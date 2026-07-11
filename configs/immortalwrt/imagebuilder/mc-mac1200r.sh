42b92885c7e1f14ddc5497e4af9a62d2bd0610ad
sieucaocao <144571307+sieucaocao@users.noreply.github.com>
2026-07-11 20:32:31 +0700

    Update mc-mac1200r.sh

--- configs/immortalwrt/imagebuilder/mc-mac1200r.sh
+++ configs/immortalwrt/imagebuilder/mc-mac1200r.sh
@@ -6,4 +6,4 @@
 make image \
      FILES=files \
      PROFILE=mc-mac1200r \
-     PACKAGES="-block-mount -luci-app-filetransfer -luci-app-cpufreq"
+     PACKAGES="-block-mount -luci-app-filetransfer -luci-app-cpufreq -ath10k-firmware-qca988x -kmod-ath10k ath10k-firmware-qca988x-ct kmod-ath10k-smallbuffers"
